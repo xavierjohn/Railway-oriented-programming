8784925eb39140dd19c02c72cb76f55097abf99a
Xavier John <xavierjohn@hotmail.com>
2017-08-04 10:11:09 -0700

    Added tests for == and !=

--- test/DomainDrivenDesignTests/ValueObject{T}Spec.cs
+++ test/DomainDrivenDesignTests/ValueObject{T}Spec.cs
@@ -1,5 +1,5 @@
-﻿using CWiz.DomainDrivenDesign;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using CWiz.DomainDrivenDesign;
 using Xunit;
 
 namespace DomainDrivenDesignTests
@@ -23,38 +23,74 @@ namespace DomainDrivenDesignTests
     public class ValueObject_T_Spec
     {
         [Fact]
-        public void Two_different_instances_of_a_value_object_with_same_values_should_be_equal()
+        public void Equal_should_return_true_for_two_different_instances_with_same_values()
         {
             var fname = "Xavier";
             var lname = "John";
             var age = 99;
-            var phoneNumbers = new List<string>() { "123-456-7890", "234-567-8901" };
+            var phoneNumbers = new List<string> {"123-456-7890", "234-567-8901"};
             var instance1 = new Person(fname, lname, age, phoneNumbers);
             var instance2 = new Person(fname, lname, age, phoneNumbers);
             Assert.Equal(instance1, instance2);
         }
 
         [Fact]
-        public void Two_value_object_with_different_values_should_be_different()
+        public void NotEqual_should_return_true_when_values_are_different()
         {
-            var phoneNumbers = new List<string>() { "123-456-7890", "234-567-8901" };
+            var phoneNumbers = new List<string> {"123-456-7890", "234-567-8901"};
             var instance1 = new Person("Xavier", "John", 99, phoneNumbers);
             var instance2 = new Person("Space", "Ghost", 99, phoneNumbers);
             Assert.NotEqual(instance1, instance2);
         }
 
         [Fact]
-        public void Two_value_objects_with_different_only_in_the_collection_should_be_different()
+        public void X3DX3D_should_return_true_for_two_different_instances_with_same_value()
         {
             var fname = "Xavier";
             var lname = "John";
             var age = 99;
-            var phoneNumbers1 = new List<string>() { "123-456-7890", "234-567-8901" };
-            var phoneNumbers2 = new List<string>() { "123-456-7890" };
+            var phoneNumbers = new List<string> {"123-456-7890", "234-567-8901"};
+            var instance1 = new Person(fname, lname, age, phoneNumbers);
+            var instance2 = new Person(fname, lname, age, phoneNumbers);
+            Assert.True(instance1 == instance2);
+        }
+
+        [Fact]
+        public void ne_should_return_true_when_values_are_different()
+        {
+            var phoneNumbers = new List<string> {"123-456-7890", "234-567-8901"};
+            var instance1 = new Person("Xavier", "John", 99, phoneNumbers);
+            var instance2 = new Person("Space", "Ghost", 99, phoneNumbers);
+            Assert.True(instance1 != instance2);
+        }
+
+
+        [Fact]
+        public void NotEqual_should_return_true_when_only_the_collections_are_different()
+        {
+            var fname = "Xavier";
+            var lname = "John";
+            var age = 99;
+            var phoneNumbers1 = new List<string> {"123-456-7890", "234-567-8901"};
+            var phoneNumbers2 = new List<string> {"123-456-7890"};
 
             var instance1 = new Person(fname, lname, age, phoneNumbers1);
             var instance2 = new Person(fname, lname, age, phoneNumbers2);
             Assert.NotEqual(instance1, instance2);
+        }
+
+        [Fact]
+        public void ne_should_return_true_when_only_the_collections_are_different()
+        {
+            var fname = "Xavier";
+            var lname = "John";
+            var age = 99;
+            var phoneNumbers1 = new List<string> {"123-456-7890", "234-567-8901"};
+            var phoneNumbers2 = new List<string> {"123-456-7890"};
+
+            var instance1 = new Person(fname, lname, age, phoneNumbers1);
+            var instance2 = new Person(fname, lname, age, phoneNumbers2);
+            Assert.True(instance1 != instance2);
         }
     }
 }
